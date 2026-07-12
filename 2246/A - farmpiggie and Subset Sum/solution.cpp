662b522531fb9a55288ab7d02da94fe5d9349424
Aditya Santosh Wadje <f20241335@pilani.bits-pilani.ac.in>
2026-07-12 20:16:21 +0530

    Add farmpiggie and Subset Sum [A] from Codeforces

---
+++ 2246/A - farmpiggie and Subset Sum/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol(){
+    int n;
+    cin>>n;
+    vector<int> p(n);
+    for(int i=0; i<n;i++)
+        p[i]=n-i;
+    for(int i=0; i<n;i++)
+        cout<<p[i]<<" ";
+    cout<<endl;
+ 
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+        sol();
+    return 0;
+}
